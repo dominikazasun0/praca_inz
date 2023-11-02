d30578dd1e22a62e28c34f2139f44367e1ea4638
Dominika <dominika.zasun.stud@pw.edu.pl>
2023-11-02 14:32:35 +0100

    symulacje metody próbkowania bez aproksymacji

--- pluto/kody_pluto+gen/metoda_próbkowania+prosta_gen.py
+++ pluto/kody_pluto+gen/metoda_próbkowania+prosta_gen.py
@@ -45,87 +45,102 @@ sdr.rx_enabled_channels = [0 ,1] # dwa kanały odbiorcze włączone
 
 #Ustawienia generatora (do wykresu)
 moc =-40
-fg=800200000
+fg=800010000
 faza=0
+#ilość_przebiegów_na_wykresie=
 
 result_seria=[]
 # Odbiór danych 20 razy (20 x N próbek)
 for m in range(200) :
-    data = sdr.rx()
+    if m % 5 == 0:
+        data = sdr.rx()
 
-#Podział odebranych danych na kanał 1 i 0
-    Rx_0=data[0]
-    Rx_1=data[1] 
-    prev=Rx_0[0]
-    prev1=Rx_1[0]
+    #Podział odebranych danych na kanał 1 i 0
+        Rx_0=data[0]
+        Rx_1=data[1] 
+        prev=Rx_0[0]
+        prev1=Rx_1[0]
 
-    for i  in range(0,len(Rx_0)):
-        if Rx_0[i].real == 0 and prev.real > 0:
-            tabela0.append(i)
-            tabela0_1.append(Rx_0[i].real)
-        elif Rx_0[i].real <= 0 and prev.real > 0 :
-            tabela0.append(oblicz_prosta(i-1,prev.real,i,Rx_0[i].real))
-            tabela0_1.append(0)
-        prev=Rx_0[i]
+        for i  in range(0,len(Rx_0)):
+            if Rx_0[i].real == 0 and prev.real > 0:
+                tabela0.append(i)
+                tabela0_1.append(Rx_0[i].real)
+            elif Rx_0[i].real <= 0 and prev.real > 0 :
+                tabela0.append(oblicz_prosta(i-1,prev.real,i,Rx_0[i].real))
+                tabela0_1.append(0)
+            prev=Rx_0[i]
 
-    for j  in range(0,len(Rx_1)):
-        if Rx_1[j].real == 0 and prev1.real > 0:
-            tabela1.append(j)
-            tabela1_1.append(Rx_1[j].real)
-        elif Rx_1[j].real <= 0 and prev1.real > 0 :
-            tabela1.append(oblicz_prosta(j-1,prev1.real,j,Rx_1[j].real))
-            tabela1_1.append(0)
-        prev1=Rx_1[j]
-
-
-    #próbki_na_okres=(1/fc)/(1/sdr.sample_rate)
-    próbki_na_okres=tabela0[1]-tabela0[0]
-    '''
-    x = range(tabela0[0], tabela0[0] + 6*int(próbki_na_okres))
-    plt.plot(x, Rx_0[tabela0[0]:tabela0[0] + 6*int(próbki_na_okres)])
-    plt.scatter(tabela0[0:7], tabela0_1[0:7], color='red', label='Zero Crossing', marker='o')
+        for j  in range(0,len(Rx_1)):
+            if Rx_1[j].real == 0 and prev1.real > 0:
+                tabela1.append(j)
+                tabela1_1.append(Rx_1[j].real)
+            elif Rx_1[j].real <= 0 and prev1.real > 0 :
+                tabela1.append(oblicz_prosta(j-1,prev1.real,j,Rx_1[j].real))
+                tabela1_1.append(0)
+            prev1=Rx_1[j]
 
 
+        #próbki_na_okres=(1/fc)/(1/sdr.sample_rate)
+        próbki_na_okres=tabela0[1]-tabela0[0]
+        '''
+        chan0_plot_start = int(tabela0[0])
+        chan0_plot_stop = int(tabela0[0]) + int(6*próbki_na_okres)
+        x = range(chan0_plot_start,chan0_plot_stop)
+        plt.plot(x, Rx_0[chan0_plot_start:chan0_plot_stop])
+        plt.scatter(tabela0[0:7], tabela0_1[0:7], color='red', label='Zero Crossing', marker='o')
 
-    x = range(tabela1[0], tabela1[0] + 6 * int(próbki_na_okres))
-    plt.plot(x, Rx_1[tabela1[0]:tabela1[0] + 6*int(próbki_na_okres)])
-    plt.scatter(tabela1[0:7], tabela1_1[0:7], color='black', label='Zero Crossing', marker='o')
-    plt.grid()
-    plt.show()
-    '''
-    #print(tabela0[0:5])
-    #print(tabela1[0:5])
+        x = range(tabela0[0], tabela0[0] + 6*int(próbki_na_okres))
+        plt.plot(x, Rx_0[tabela0[0]:tabela0[0] + 6*int(próbki_na_okres)])
+        plt.scatter(tabela0[0:7], tabela0_1[0:7], color='red', label='Zero Crossing', marker='o')
 
-    #print(len(tabela1))
-    #print(len(tabela0))
-    result = []
-    
-    if tabela0[0] < tabela1[0] :
-        for i in range(len(tabela1)-150) :
-            result.append((tabela1[i] - tabela0[i])*(360/próbki_na_okres))
-    else :
-        for j in range(20) :
-            result.append((tabela1[j+1] - tabela0[j])*(360/próbki_na_okres))
-    #print('Różnica fazy w stopniach:',np.mean(result))    
-    #plt.title(f'LO={sdr.rx_lo} fs={sdr.sample_rate} fc={fc}')
-    result_seria.append(np.mean(result))
-    
-    tabela0=[]
-    tabela1=[]
-    tabela1_1=[]
-    tabela0_1=[]
+
+
+        x = range(tabela1[0], tabela1[0] + 6 * int(próbki_na_okres))
+        plt.plot(x, Rx_1[tabela1[0]:tabela1[0] + 6*int(próbki_na_okres)])
+        plt.scatter(tabela1[0:7], tabela1_1[0:7], color='black', label='Zero Crossing', marker='o')
+        
+        plt.grid()
+        plt.show()
+        '''
+        #print(tabela0[0:5])
+        #print(tabela1[0:5])
+
+        #print(len(tabela1))
+        #print(len(tabela0))
+        result = []
+        
+        if tabela0[0] < tabela1[0] :
+            for i in range(len(tabela1)-5) :
+                result.append((tabela1[i] - tabela0[i])*(360/próbki_na_okres))
+        else :
+            for j in range(len(tabela1)-5) :
+                result.append((tabela1[j+1] - tabela0[j])*(360/próbki_na_okres))
+        print('Różnica fazy w stopniach:',np.mean(result))    
+        #plt.title(f'LO={sdr.rx_lo} fs={sdr.sample_rate} fc={fc}')
+        result_seria.append(np.mean(result))
+        
+        tabela0=[]
+        tabela1=[]
+        tabela1_1=[]
+        tabela0_1=[]
+        time.sleep(4)
+    else:
+        data=[]      
 
 plt.plot(result_seria)
-plt.title('Metod z aproksymacją - fg={}, fs={}, moc={}, faza={}, LO={}'.format(fg, sdr.sample_rate, moc, faza, sdr.rx_lo))
+plt.title('Metod z aproksymacją - fg={}, fs={},\nmoc={}, faza=zmiana o 1° od 0° do 45° na chan2'.format(fg, sdr.sample_rate, moc))
+plt.xlabel("Iteracja odbioru próbek")
+plt.ylabel("Różnica fazy [°]")
+plt.savefig('sym_18.svg', format='svg')
 plt.grid()
 plt.show()
-'''
 
-with open('result_seria.txt', 'w') as plik:
+
+with open('rsym_18.txt', 'w') as plik:
     # Zapisz dane do pliku
     for element in result_seria:
         plik.write(str(element) + '\n')
-
+'''
 with open('chanel1.txt', 'w') as plik:
     # Zapisz dane do pliku
     for element in Rx_1:
