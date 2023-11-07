31cf991bb5bc2f416210aa19959d2255e6af118a
Dominika <dominika.zasun.stud@pw.edu.pl>
2023-11-07 17:46:30 +0100

    Poprawienie metody wyboru punktu początkowego i testowanie

--- pluto/kody_pluto+gen/metoda_próbkowania+prosta_gen.py
+++ pluto/kody_pluto+gen/metoda_próbkowania+prosta_gen.py
@@ -23,120 +23,144 @@ def oblicz_prosta(x1, y1, x2, y2):
     
     return x_intercept
 
+#https://github.com/analogdevicesinc/pyadi-iio/blob/master/examples/ad9361_example.py
+# Copyright (C) 2022 Analog Devices, Inc.
+#
+# SPDX short identifier: ADIBSD
+import adi
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import signal
+import time
+#import tkinter as tk
 
-tabela0=[]
-tabela0_1=[]
-tabela1=[]
-tabela1_1=[]
-# Przed użyciem kodu należy ustawić jego tryb pracy na podwójny odbiór i podwójne nadawania
+tabela0=[]      # Miejsca zerowe chan0
+tabela0_1=[]    # Wartości miejsc zerowych chan0
+tabela1=[]      # Miejsca zerowe chan1
+tabela1_1=[]    # Wartości miejsc zerowych chan1
+result_seria=[] # Zapis wyników pomiaru
+result = []
 
-sdr = adi.ad9361(uri="ip:192.168.2.1") #Tworzenie radia
+#Dane do wykresu
+moc =-40     # Moc sygnału
+fg=400020000 # Częstotliwość ustawiona na generatorze
+przebieg=6   # Liczba okresów sygnału jakie chcemy zobaczyć
 
 # Konfigurowanie własności transmisji
+sdr = adi.ad9361(uri="ip:192.168.2.1") #Tworzenie radia
 sdr.rx_rf_bandwidth = 1000000 # szerokość pasma odbiornika
 sdr.sample_rate = 10000000 # częstotliwość próbkowania
-sdr.rx_lo = 800000000 # częstotliwość LO odbiornika
-sdr.gain_control_mode_chan0 = "fast_attack"
-sdr.gain_control_mode_chan1 = "fast_attack"
+sdr.rx_lo = 400000000 # częstotliwość LO odbiornika
+sdr.gain_control_mode_chan0 = "slow_attack"
+sdr.gain_control_mode_chan1 = "slow_attack"
 sdr.rx_buffer_size = 32768
-
-# Konfiguracja kanałów dbiorczych
 sdr.rx_enabled_channels = [0 ,1] # dwa kanały odbiorcze włączone
 
-#Ustawienia generatora (do wykresu)
-moc =-40
-fg=800010000
-faza=0
-#ilość_przebiegów_na_wykresie=
 
-result_seria=[]
-# Odbiór danych 20 razy (20 x N próbek)
+# Odbiór danych
 for m in range(200) :
-    if m % 5 == 0:
-        data = sdr.rx()
+    data = sdr.rx()
 
-    #Podział odebranych danych na kanał 1 i 0
-        Rx_0=data[0]
-        Rx_1=data[1] 
-        prev=Rx_0[0]
-        prev1=Rx_1[0]
-
+    if m % 5 == 0: # Analizuję co piąty zestaw próbek, bo co piąty zestaw próbek przenoszą się zmiany ustawień generatora
+#Podział odebranych danych na kanał 1 i 0
+        
+        # Poszukiwanie punktu w którym sygnały się przecinają na początku sygnału i wybranie go jako początkowy punkt analizy
+        subst=abs(data[0].real-data[1].real)
+        min_val=subst[0]
+        for a in range(1,1000):
+            if (subst[a] < min_val) :
+                min_val=subst[a]
+                min_index=a
+        Rx_0 = np.array(data[0][min_index:len(data[0])-200])
+        Rx_1 = np.array(data[1][min_index:len(data[1])-200])
+        
+        
+        # Detekcja miejsc zerowych
+        prev_0 = Rx_0[0]
+        prev_1 = Rx_1[0]
+        
         for i  in range(0,len(Rx_0)):
-            if Rx_0[i].real == 0 and prev.real > 0:
+            if Rx_0[i].real == 0 and prev_0.real > 0:
                 tabela0.append(i)
                 tabela0_1.append(Rx_0[i].real)
-            elif Rx_0[i].real <= 0 and prev.real > 0 :
-                tabela0.append(oblicz_prosta(i-1,prev.real,i,Rx_0[i].real))
+            elif Rx_0[i].real <= 0 and prev_0.real > 0 :
+                tabela0.append(oblicz_prosta(i-1,prev_0.real,i,Rx_0[i].real))
                 tabela0_1.append(0)
-            prev=Rx_0[i]
+            prev_0=Rx_0[i]
 
         for j  in range(0,len(Rx_1)):
-            if Rx_1[j].real == 0 and prev1.real > 0:
+            if Rx_1[j].real == 0 and prev_1.real > 0:
                 tabela1.append(j)
                 tabela1_1.append(Rx_1[j].real)
-            elif Rx_1[j].real <= 0 and prev1.real > 0 :
-                tabela1.append(oblicz_prosta(j-1,prev1.real,j,Rx_1[j].real))
+            elif Rx_1[j].real <= 0 and prev_1.real > 0 :
+                tabela1.append(oblicz_prosta(j-1,prev_1.real,j,Rx_1[j].real))
                 tabela1_1.append(0)
-            prev1=Rx_1[j]
-
-
-        #próbki_na_okres=(1/fc)/(1/sdr.sample_rate)
+            prev_1=Rx_1[j]
+        
         próbki_na_okres=tabela0[1]-tabela0[0]
-        '''
-        chan0_plot_start = int(tabela0[0])
-        chan0_plot_stop = int(tabela0[0]) + int(6*próbki_na_okres)
-        x = range(chan0_plot_start,chan0_plot_stop)
-        plt.plot(x, Rx_0[chan0_plot_start:chan0_plot_stop])
-        plt.scatter(tabela0[0:7], tabela0_1[0:7], color='red', label='Zero Crossing', marker='o')
 
-        x = range(tabela0[0], tabela0[0] + 6*int(próbki_na_okres))
-        plt.plot(x, Rx_0[tabela0[0]:tabela0[0] + 6*int(próbki_na_okres)])
-        plt.scatter(tabela0[0:7], tabela0_1[0:7], color='red', label='Zero Crossing', marker='o')
-
-
-
-        x = range(tabela1[0], tabela1[0] + 6 * int(próbki_na_okres))
-        plt.plot(x, Rx_1[tabela1[0]:tabela1[0] + 6*int(próbki_na_okres)])
-        plt.scatter(tabela1[0:7], tabela1_1[0:7], color='black', label='Zero Crossing', marker='o')
         
-        plt.grid()
-        plt.show()
-        '''
-        #print(tabela0[0:5])
-        #print(tabela1[0:5])
+        # Obliczenie różnicy fazy
 
-        #print(len(tabela1))
-        #print(len(tabela0))
-        result = []
-        
         if tabela0[0] < tabela1[0] :
-            for i in range(len(tabela1)-5) :
+            for i in range(min(len(tabela0)),len(tabela1)) :
                 result.append((tabela1[i] - tabela0[i])*(360/próbki_na_okres))
         else :
-            for j in range(len(tabela1)-5) :
-                result.append((tabela1[j+1] - tabela0[j])*(360/próbki_na_okres))
-        print('Różnica fazy w stopniach:',np.mean(result))    
-        #plt.title(f'LO={sdr.rx_lo} fs={sdr.sample_rate} fc={fc}')
-        result_seria.append(np.mean(result))
+            for j in range(min(len(tabela0)),len(tabela1)) :
+                result.append((tabela0[j] - tabela1[j])*(360/próbki_na_okres))
         
+        print('Różnica fazy w stopniach:',np.mean(result))
+        
+        result_seria.append(np.mean(result))
+
+        # Tworzenie wykresów
+        
+        chan0_plot_start = int(tabela0[0])
+        chan0_plot_stop = int(tabela0[0]) + int(przebieg*próbki_na_okres)
+
+        chan1_plot_start = int(tabela1[0])
+        chan1_plot_stop = int(tabela1[0]) + int(przebieg*próbki_na_okres)
+
+        x = range(chan0_plot_start, chan0_plot_stop)
+        plt.plot(x, Rx_0[chan0_plot_start:chan0_plot_stop],'k',label='Chan0')
+        plt.scatter(tabela0[0:przebieg+1], tabela0_1[0:przebieg+1], color='k', marker='o')
+
+        x = range(chan1_plot_start, chan1_plot_stop)
+        plt.plot(x, Rx_1[chan1_plot_start:chan1_plot_stop], 'r', label='Chan1')
+        plt.scatter(tabela1[0:przebieg+1], tabela1_1[0:przebieg+1], color='r', marker='o')
+        plt.legend(loc='upper left')
+        plt.title(f'diff={np.mean(result)}')
+        plt.grid()
+        plt.show()
+        
+        plt.plot(Rx_0[:1500])
+        plt.plot(Rx_1[:1500])
+        plt.grid()
+        plt.show()
+        
+        
+        print("\n")
+        
+        # Usuwanie danych po pomiarze
+        result = []
         tabela0=[]
         tabela1=[]
         tabela1_1=[]
         tabela0_1=[]
-        time.sleep(4)
-    else:
-        data=[]      
+        time.sleep(2)
 
-plt.plot(result_seria)
-plt.title('Metod z aproksymacją - fg={}, fs={},\nmoc={}, faza=zmiana o 1° od 0° do 45° na chan2'.format(fg, sdr.sample_rate, moc))
+    else:
+        data=[]    
+
+plt.plot(result_seria,marker='o', linestyle='-')
+plt.title('Metod bez aproksymacji - fg={}, fs={},\nmoc={}, faza=90'.format(fg, sdr.sample_rate, moc))
 plt.xlabel("Iteracja odbioru próbek")
 plt.ylabel("Różnica fazy [°]")
-plt.savefig('sym_18.svg', format='svg')
 plt.grid()
+plt.savefig('sym_24.svg', format='svg')
 plt.show()
 
-
-with open('rsym_18.txt', 'w') as plik:
+with open('sym_24.txt', 'w') as plik:
     # Zapisz dane do pliku
     for element in result_seria:
         plik.write(str(element) + '\n')
--- pluto/kody_pluto+gen/metoda_próbkowania_gen.py
+++ pluto/kody_pluto+gen/metoda_próbkowania_gen.py
@@ -14,17 +14,18 @@ tabela0_1=[]    # Wartości miejsc zerowych chan0
 tabela1=[]      # Miejsca zerowe chan1
 tabela1_1=[]    # Wartości miejsc zerowych chan1
 result_seria=[] # Zapis wyników pomiaru
+result = []
 
 #Dane do wykresu
 moc =-40     # Moc sygnału
-fg=800010000 # Częstotliwość ustawiona na generatorze
+fg=400040000 # Częstotliwość ustawiona na generatorze
 przebieg=6   # Liczba okresów sygnału jakie chcemy zobaczyć
 
 # Konfigurowanie własności transmisji
 sdr = adi.ad9361(uri="ip:192.168.2.1") #Tworzenie radia
 sdr.rx_rf_bandwidth = 1000000 # szerokość pasma odbiornika
 sdr.sample_rate = 10000000 # częstotliwość próbkowania
-sdr.rx_lo = 800000000 # częstotliwość LO odbiornika
+sdr.rx_lo = 400000000 # częstotliwość LO odbiornika
 sdr.gain_control_mode_chan0 = "slow_attack"
 sdr.gain_control_mode_chan1 = "slow_attack"
 sdr.rx_buffer_size = 32768
@@ -32,18 +33,26 @@ sdr.rx_enabled_channels = [0 ,1] # dwa kanały odbiorcze włączone
 
 
 # Odbiór danych
-for m in range(225) :
+for m in range(200) :
     data = sdr.rx()
 
     if m % 5 == 0: # Analizuję co piąty zestaw próbek, bo co piąty zestaw próbek przenoszą się zmiany ustawień generatora
 #Podział odebranych danych na kanał 1 i 0
-        Rx_0 = data[0][200:len(data[0])-200]
-        Rx_1 = data[1][200:len(data[1])-200]
+        Rx_0 = np.array(data[0][200:len(data[0])-200])
+        Rx_1 = np.array(data[1][200:len(data[1])-200])
+        
+        '''
+        while (Rx_0[0]>=0 and Rx_1[0]<=0) or (Rx_0[0]<=0 and Rx_1[0]>=0) :
+            if Rx_0[0] >=0 :
+                Rx_0[0:50] = np.full(50,-1)
+            if Rx_1[0] >=0 :    
+                Rx_1[0:50] = np.full(50,-1)
+            print("Robię dobrą robotę")
+        '''
+        
         prev_0 = Rx_0[0]
         prev_1 = Rx_1[0]
-        result = []
         
-
         # Detekcja miejsc zerowych
         for i  in range(0,len(Rx_0)):
             if Rx_0[i].real <= 0 and prev_0.real > 0 :
@@ -58,6 +67,30 @@ for m in range(225) :
             prev_1=Rx_1[j]
         
         próbki_na_okres=tabela0[1]-tabela0[0]
+        
+        if Rx_0[0] > Rx_0[1] :
+            if not((Rx_0[0] < 0 and Rx_1[0] < 0 ) or (Rx_0[0] > 0 and Rx_1[0] > 0 )):
+                print("Będzie błąd")
+                if Rx_0[0] > 0 :
+                    del tabela0[0]
+                if Rx_1[0] > 0:
+                    del tabela1[0]   
+
+        
+        #print(len(tabela1))
+        #print(len(tabela0))
+        
+        # Obliczenie różnicy fazy
+        if tabela0[0] < tabela1[0] :
+            for i in range(len(tabela1)-100) :
+                result.append((tabela1[i] - tabela0[i])*(360/próbki_na_okres))
+        else :
+            for j in range(len(tabela1)-100) :
+                result.append((tabela0[j] - tabela1[j])*(360/próbki_na_okres))
+        
+        print('Różnica fazy w stopniach:',np.mean(result))
+        
+        result_seria.append(np.mean(result))
         
         # Tworzenie wykresów
         chan0_plot_start = tabela0[0]
@@ -74,45 +107,37 @@ for m in range(225) :
         plt.plot(x, Rx_1[chan1_plot_start:chan1_plot_stop], 'r', label='Chan1')
         plt.scatter(tabela1[0:przebieg+1], tabela1_1[0:przebieg+1], color='r', marker='o')
         plt.legend(loc='upper left')
-        plt.title("Przebiegi sygnałów")
+        plt.title(f'diff={np.mean(result)}')
         plt.grid()
         plt.show()
-        #print(tabela0[0:5])
-        #print(tabela1[0:5])
-
-        #print(len(tabela1))
-        #print(len(tabela0))
         
-        '''
-        if tabela0[0] < tabela1[0] :
-            for i in range(len(tabela1)) :
-                result.append((tabela1[i] - tabela0[i])*(360/próbki_na_okres))
-        else :
-            for j in range(len(tabela1)) :
-                result.append((tabela1[j+1] - tabela0[j])*(360/próbki_na_okres))
-        print('Różnica fazy w stopniach:',np.mean(result))
-        #plt.title(f'LO={sdr.rx_lo} fs={sdr.sample_rate} fc={fc}')
-        result_seria.append(np.mean(result))
-        '''
+        plt.plot(Rx_0)
+        plt.plot(Rx_1)
+        plt.grid()
+        plt.show()
+        
+        print("\n")
+        
         # Usuwanie danych po pomiarze
+        result = []
         tabela0=[]
         tabela1=[]
         tabela1_1=[]
         tabela0_1=[]
-        time.sleep(4)
+        time.sleep(1)
 
     else:
         data=[]    
 
 plt.plot(result_seria,marker='o', linestyle='-')
-plt.title('Metod bez aproksymacji - fg={}, fs={},\nmoc={}, faza=zmiana o 1° od 325° do 280° na chan2'.format(fg, sdr.sample_rate, moc))
+plt.title('Metod bez aproksymacji - fg={}, fs={},\nmoc={}, faza=90'.format(fg, sdr.sample_rate, moc))
 plt.xlabel("Iteracja odbioru próbek")
 plt.ylabel("Różnica fazy [°]")
 plt.grid()
-plt.savefig('sym_17.svg', format='svg')
+plt.savefig('sym_21.svg', format='svg')
 plt.show()
 
-with open('sym_17.txt', 'w') as plik:
+with open('sym_21.txt', 'w') as plik:
     # Zapisz dane do pliku
     for element in result_seria:
         plik.write(str(element) + '\n')
